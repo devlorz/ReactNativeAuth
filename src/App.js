2fda9bc6c885b37713f7d19f4dfa8aea2dceccc5
Nutti Saelor <leelorz6@gmail.com>
2018-03-14 16:48:58 +0700

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import { firebaseKey } from '../key';
 
 class App extends Component {
+  componentWillMount() {
+    console.log(firebaseKey);
+    firebase.initializeApp(firebaseKey);
+  }
+
   render() {
     return (
       <View>
